7a912a3cec4693c8f819d36db5cad1af94455ac0
bojavou <134245276+bojavou@users.noreply.github.com>
2023-07-11 05:16:23 +0000

    Detect ESM syntax in CommonJS module

--- bin/inspect.mjs
+++ bin/inspect.mjs
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
 import ChangeStream from '#lib/stream/change.mjs'
+import validate from '#lib/validate.mjs'
 
 const changes = new ChangeStream()
+
+let compressed = false
 for await (const change of changes) {
   if (change.deleted) {
     console.log('DELETED:', change.id)
+    compressed = true
     continue
+  } else {
+    if (compressed) console.log()
+    compressed = false
+    await validate(change.id)
+    console.log()
   }
-  console.log(change.id)
 }
