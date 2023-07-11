e0353345d83862a15beec52d52406f6726465bfa
bojavou <134245276+bojavou@users.noreply.github.com>
2023-07-11 05:27:02 +0000

    Accept token option

--- bin/inspect.mjs
+++ bin/inspect.mjs
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 import ChangeStream from '#lib/stream/change.mjs'
+import commandLineArgs from 'command-line-args'
 import log from '#lib/log.mjs'
 import validate from '#lib/validate.mjs'
 
-const changes = new ChangeStream()
+const options = commandLineArgs([
+  { name: 'token', alias: 't', type: Number }
+])
+
+const changes = new ChangeStream(options)
 
 for await (const change of changes) {
   if (change.deleted) {
